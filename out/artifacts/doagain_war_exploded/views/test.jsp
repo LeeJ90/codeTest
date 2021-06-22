1fdad5259358529d7b259864a23069befd389b8d
이지성 <cocoa@mqnic.com>
2021-06-22 17:05:52 +0900

    init

---
+++ out/artifacts/doagain_war_exploded/views/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ijiseong
+  Date: 2021/04/29
+  Time: 10:28 오전
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+asfdasfdadfasfsadfasdfdsfa
+</body>
+</html>
---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+    @RequestMapping("/")
+    public String testPage(){
+        return "test";
+    }
+
+}
